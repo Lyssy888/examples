f684f79ade755a093cfc1aaf666b88f7e96dfb3b
Ruslan <888lyssy@gmail.com>
2023-01-31 17:29:56 +0500

    задача1

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 1.//Напишите программу, которая на вход принимает число и выдаёт его квадрат
+Console.Write(" input you number: "); // запрос данных у пользователя
+
+int number = Convert.ToInt32(Console.ReadLine()); // в переменную Number записали введеную пользователем значение
+
+Console.WriteLine($" You number is {number}");
+int result = number * number;
+Console.WriteLine($" квадрат {number} - {result}");
+Console.WriteLine(number*number);
