fa90c94d182a51815c5e9f5e98acd180b1bb302c
Ruslan <888lyssy@gmail.com>
2023-01-31 19:12:06 +0500

    задача3

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
-﻿//Напишите программу, которая на вход принимает два числа и проверяет,//является ли первое число квадратом второго.
-Console.Write(" input you first number: ");
-int number_1 = Convert.ToInt32(Console.ReadLine());
+﻿//Задача 3.
+//Напишите программу, которая на вход принимает два числа и проверяет,
+//является ли первое число квадратом второго.
 
-Console.Write(" input you second number: ");
-int number_2 = Convert.ToInt32(Console.ReadLine());
+Console.Write( "input your number N: ");
+int num = Convert.ToInt32(Console.ReadLine());
 
-if (number_1 == number_2 * number_2) // проверка на равенство
-    Console.WriteLine($"число {number_1} является квадратом числа {number_2}");// опустить фигурные скобки можно если одно действие
+int current_num = (-1) * num;
 
-else
+while( current_num <= num)
 {
-    Console.WriteLine($"число {number_1} не является квадратом числа {number_2}");
+    Console.Write(current_num + " ");
+    current_num++;
 }
